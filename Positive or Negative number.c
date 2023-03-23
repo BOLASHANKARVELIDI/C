da2af43e79c4504d408b2b3f62530a1f07d2a7d8
BOLA SHANKAR VELIDI <110627239+BOLASHANKARVELIDI@users.noreply.github.com>
2023-03-23 22:22:59 +0530

    Update Positive or Negative number.c

--- Positive or Negative number.c
+++ Positive or Negative number.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int num = 23;
+    int num;
+    Printf("Enter the Number: ");
+    Scanf("%d",&a);
       
     //Conditions to check if the number is negative/positive or zero
     if (num > 0)
